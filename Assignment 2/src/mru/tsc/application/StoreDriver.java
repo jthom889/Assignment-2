3698857564f9a2e1b31ec1e06e92a046e1446a0b
AlliVictor <valli978@mtroyal.ca>
2022-03-07 12:52:31 -0700

    Created the StoreDriver, StoreMenu and test classes

---
+++ Assignment 2/src/mru/tsc/application/StoreDriver.java
@@ -0,0 +1,11 @@
+package mru.tsc.application;
+
+public class StoreDriver 
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		//Calls the instance of GameManager to run the whole program
+		//new whatever class();
+	}
+}
---
+++ Assignment 2/src/mru/tsc/view/StoreMenu.java
@@ -0,0 +1,6 @@
+package mru.tsc.view;
+
+public class StoreMenu 
+{
+
+}
